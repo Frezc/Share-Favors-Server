a33924e28e57380045aa7afff2a1b4575e169b6f
frezc <504021398@qq.com>
2016-03-29 17:26:11 +0800

    修改邮件验证页面

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -3,11 +3,10 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
         <style>
             html, body {
                 height: 100%;
+                font-family: 'Roboto','Helvetica Neue','Microsoft Yahei','Hiragino Sans GB','Microsoft Sans Serif','WenQuanYi Micro Hei',sans-serif;
             }
 
             body {
@@ -16,29 +15,35 @@
                 width: 100%;
                 display: table;
                 font-weight: 100;
-                font-family: 'Lato';
             }
 
             .container {
-                text-align: center;
-                display: table-cell;
                 vertical-align: middle;
             }
 
             .content {
-                text-align: center;
+                margin: 8px;
                 display: inline-block;
             }
 
             .title {
-                font-size: 96px;
+                font-size: 16px;
             }
+
+            .code {
+                margin: 8px;
+                font-size: 24px;
+                font-weight: 500;
+            }
+
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">你的请求{{$email}}已通过，验证码为{{$code}}</div>
+                <div class="title">你向邮箱{{$email}}请求的验证码为</div>
+                <div class="code">{{$code}}</div>
+                <div>请尽快使用，如果不是你本人操作请无视</div>
             </div>
         </div>
     </body>
